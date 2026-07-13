05cdedc89406b675d8238a8488abd97bd5ccfc08
14CES <2025111968@dgu.ac.kr>
2026-07-13 23:07:07 +0900

    feat : Tag 기능 구현 및 tag로 Singer 찾기 기능 구현

--- singer/serializers.py
+++ singer/serializers.py
@@ -5,17 +5,28 @@ class SingerSerializer(serializers.ModelSerializer):
     id = serializers.CharField(read_only=True)
 
     songs = serializers.SerializerMethodField(read_only=True)
+    tags = serializers.SerializerMethodField(read_only=True)
+    image = serializers.ImageField(use_url=True, required=False)
 
     def get_songs(self, instance):
         serializer = SongSerializer(instance.songs, many=True)
         return serializer.data
+    
+    def get_tags(self, instance):
+        tags = instance.tags.all()
+        return [tag.name for tag in tags]
 
     class Meta:
         model = Singer
-        fields = ['id', 'name', 'content', 'debut', 'songs']
+        fields = ['id', 'name', 'content', 'debut', 'tags', 'image', 'songs']
 
 class SongSerializer(serializers.ModelSerializer):
     class Meta:
         model = Song
         fields = '__all__'
         read_only_fields = ['singer']
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
--- singer/urls.py
+++ singer/urls.py
@@ -2,10 +2,16 @@ from django.urls import path
 from .views import *
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 app_name = 'singer'
 
 urlpatterns = [
     path('singer', views.singer_list_create),
     path('singer/<int:singer_id>/song', views.song_read_create),
     path('singer/<int:singer_id>', views.singer_detail_update_delete),
+    path('tags/<str:tags_name>', views.find_tag),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
